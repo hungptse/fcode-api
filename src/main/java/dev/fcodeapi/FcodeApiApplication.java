5e2aa9847e4e1b3a331fb17fcdf5124a439a6f68
Thanh Hung <hungpt.se@gmail.com>
2018-12-09 16:12:44 +0700

    DeploY

--- src/main/java/dev/fcodeapi/FcodeApiApplication.java
+++ src/main/java/dev/fcodeapi/FcodeApiApplication.java
@@ -26,8 +26,9 @@ public class FcodeApiApplication {
                 registry.addMapping("/**")
                         .allowedOrigins("*")
                         .allowedMethods("*")
-                        .allowCredentials(true)
+                        .allowCredentials(false)
                         .allowedHeaders("Authorization","Content-Type","Access-Control-Allow-Origin")
+                        .allowedHeaders("*")
                         .exposedHeaders("Access-Control-Allow-Headers", "Authorization","Content-Type");
 
             }
