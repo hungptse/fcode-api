6dcb9f83f0a847561678d6be8bf6bcb27f56e704
Thanh Hung <hungpt.se@gmail.com>
2018-12-09 16:18:14 +0700

    Add localhost

--- src/main/java/dev/fcodeapi/FcodeApiApplication.java
+++ src/main/java/dev/fcodeapi/FcodeApiApplication.java
@@ -24,7 +24,7 @@ public class FcodeApiApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("*","localhost:3000")
                         .allowedMethods("*")
                         .allowCredentials(false)
                         .allowedHeaders("Authorization","Content-Type","Access-Control-Allow-Origin")
